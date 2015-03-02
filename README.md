afd2806bed48cf0f641a37296d67dd8801df7b4a
Fintan Ryan <fintanr@gmail.com>
2015-03-02 16:02:21 +0000

    Script to generate jekyll suitable files from our weave-gs readmes.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Generate Guide Pages for use with Jekyll #
+
+Takes a set of README.md files from subdirectorys in a repo and sets them up
+for use with jekyll, kramdown and pygment. 
+
+We read our configuration details and output file names in from a json file.
+
+Its pretty specific to a use case I currently have, but feel free to grab and use 
+as you see fit.
---
+++ genGuidesPages.pl
@@ -0,0 +1,109 @@
+#!/usr/bin/perl -w
+#
+# Generate the /guides pages
+# 
+# Takes a README.md from a directory in a repo and formats it 
+# for jekyll to use
+#
+
+use JSON;
+use Getopt::Std;
+use File::Basename;
+use constant TRUE => 1;
+use constant FALSE => 0;
+use strict;
+
+use vars qw/$opt_c $opt_g $opt_o $opt_h/;
+
+getopts("g:o:c:h");
+
+my $confFile = "guides.json";
+
+if ( !defined($opt_g) || !defined($opt_o) || defined($opt_h) ) {
+    usage();
+    exit();
+}
+
+if ( defined($opt_c) ) {
+    $confFile = $opt_c;
+}
+
+my $outDir = $opt_o;
+my $gitClone = $opt_g;
+
+my $json = loadConfigDetails($confFile);
+
+for ( my $i = 0; $i <= $#{$$json{'guides'}}; $i++ ) {
+    my $inFile = sprintf("%s/%s/README.md", $gitClone, $$json{'guides'}->[$i]->{'indir'});
+    my $outFile = sprintf("%s/%s", $outDir, $$json{'guides'}->[$i]->{'outfile'});
+    my $layout = $$json{'guides'}->[$i]->{'layout'};
+    my $markdown = $$json{'guides'}->[$i]->{'markdown'};
+    my $highlighter = $$json{'guides'}->[$i]->{'highlighter'};
+
+    my ( $title, $jekyll) = fixupReadMe($inFile);
+
+    open(OUT, ">$outFile") || die ("Unable to open $outFile");
+    printf OUT ("---\n");
+    printf OUT ("layout: %s\n", $layout);
+    printf OUT ("title: %s\n", $title);
+    printf OUT ("desciption: Weave Getting Started Guides\n");
+    printf OUT ("markdown: %s\n", $markdown);
+    printf OUT ("highlighter: %s\n", $highlighter);
+    printf OUT ("---\n\n");
+    printf OUT ("%s\n", $jekyll);
+    close(OUT);
+}
+
+    # grab the relevant readme, and fill in the various bits of 
+    # jekyllness that need to be done
+    #
+
+sub fixupReadMe {
+
+    my ( $inFile ) = @_;
+    my $title = "";
+    my $jekyll = "";
+    my $doEnd = FALSE;
+
+    open(IN, $inFile) || die ("Unable to open $inFile");
+   
+    while ( <IN> ) {
+        chomp;
+        s/\s+$//;
+        if ( /^#\s(.*)\s#/ ) {
+            $title = $1;
+        } elsif ( /^```(bash|javascript|php|java|ruby|python)$/ ) {
+            $jekyll .= "{% highlight $1 %}\n";
+            $doEnd = TRUE;
+        } elsif ( /^```$/ && $doEnd ) {
+            $jekyll .= "{% endhighlight %}\n";
+            $doEnd = FALSE;
+        } else {
+            $jekyll .= "$_\n";
+        }
+    }
+    close(IN);
+    return($title, $jekyll);
+}
+
+
+sub loadConfigDetails {
+
+    my ( $confFile ) = @_;
+     
+    local $/;
+    open( my $fh, '<', $confFile );
+    my $jsonText   = <$fh>;
+    close($fh);
+
+    my $json = decode_json( $jsonText );
+   
+    return($json);
+}
+
+sub usage {
+
+    my $name = basename($0);
+
+    printf("\nUsage: %s -g <path to local github clonse> -o <out directory> [-c <config file>][-h]\n", $name);
+}
